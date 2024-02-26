07ef46d1efac3744b77790cbc302097dad86443e
João Andrade <andradezerd@gmail.com>
2024-02-26 12:41:16 -0300

    Create Aula_001.py

---
+++ Aula_001.py
@@ -0,0 +1,16 @@
+## Declarando Variáveis
+
+## sobrenome = 'Andrade'
+## idade = '22'
+## peso = "88.5"
+
+#Exibindo as Variáveis
+
+## print (sobrenome, idade, peso)
+
+nome=input ('Qual é o seu nome?')
+idade=input ('Quantos anos você tem?')
+peso=input ('Quanto você pesa?')
+
+print (nome, idade, peso)
+
