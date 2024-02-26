022eae0ded4481bde8df967cf4b7b77ef40061cf
João Andrade <andradezerd@gmail.com>
2024-02-26 14:41:22 -0300

    Update Desafio_003.py

--- Desafios/Desafio_003.py
+++ Desafios/Desafio_003.py
@@ -1,9 +1,7 @@
 # Crie um script Python que leio dois números e tente mostrar a soma entre eles.
 
-num1=input ('Insira seu primeiro número = ')
-num2=input ('Insira seu segundo número = ')
+num1=int(input ('Insira seu primeiro número = '))
+num2=int(input ('Insira seu segundo número = '))
+resultado = num1 + num2
 
-num1=int(num1)
-num2=int(num2)
-
-print (num1 + num2)
+print ('A soma vale', resultado)
