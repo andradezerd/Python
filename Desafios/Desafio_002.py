61eb570b3af33d60abf8aad0d480a589a2db9406
João Andrade <andradezerd@gmail.com>
2024-02-26 13:50:46 -0300

    Desafios-1-3

---
+++ Desafios/Desafio_002.py
@@ -0,0 +1,8 @@
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa em mostre uma mensagem com a data formatada
+
+nome=input ('Digite seu nome = ')
+dia=input ('Dia = ')
+mes=input ('Mês = ')
+ano=input ('Ano = ')
+
+print('Olá', nome, 'você nasceu no dia', dia, 'do mês de', mes, 'no ano de', ano)
---
+++ Desafios/Desafio_003.py
@@ -0,0 +1,9 @@
+# Crie um script Python que leio dois números e tente mostrar a soma entre eles.
+
+num1=input ('Insira seu primeiro número = ')
+num2=input ('Insira seu segundo número = ')
+
+num1=int(num1)
+num2=int(num2)
+
+print (num1 + num2)
