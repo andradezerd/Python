6514149b746820fa1cea43526d12490d48cc15fd
João Andrade <andradezerd@gmail.com>
2024-03-06 13:10:08 -0300

    Aulas novas

---
+++ Aulas/Aula_011.py
@@ -0,0 +1,3 @@
+tempo = int(input('\nQuantos anos tem seu carro? '))
+
+print('Seu carro é novo' if tempo<=3 else 'Seu carro é velho')
---
+++ Aulas/Aula_012.py
@@ -0,0 +1,6 @@
+nome = str(input('\nQual é o seu nome? '))
+if nome == 'João':
+    print ('Que nome lindo você tem!')
+else:
+    print ('Seu nome é tão normal!')
+print (f'Bom dia, {nome}')
