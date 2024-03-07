ecc4d95a65954ebee91d5572c4561316977828c5
João Andrade <andradezerd@gmail.com>
2024-03-07 00:25:17 -0300

    Update Desafio_035.py

--- Desafios/Desafio_035.py
+++ Desafios/Desafio_035.py
@@ -2,10 +2,10 @@
 # E diga ao usuário se elas podem ou não formar um triângulo.
 
 reta1 = float(input('\nDigite o comprimento da PRIMEIRA reta: '))
-reta2 = float(input('\nDigite o comprimento da SEGUNDA reta: '))
-reta3 = float(input('\nDigite o comprimento da TERCEIRA reta: ')) 
+reta2 = float(input('Digite o comprimento da SEGUNDA reta: '))
+reta3 = float(input('Digite o comprimento da TERCEIRA reta: ')) 
 
 if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
-    print('É um triângulo')
+    print('\nÉ um triângulo')
 else:
-    print('Não é um triângulo')
+    print('\nNão é um triângulo')
