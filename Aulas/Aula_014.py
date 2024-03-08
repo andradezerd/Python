b5f4a6d57f9a49b742eaca9446e490e48912d536
João Andrade <andradezerd@gmail.com>
2024-03-07 23:35:20 -0300

    Atualização

---
+++ Aulas/Aula_014.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+
+cor = {'limpa':'\033[m', 'azul':'\033[34m', 'vermelho':'\033[31m'}
+
+print (f'Seja bem vindo!!, {cor["azul"]}{nome}{cor["limpa"]}!!!')
+
--- Desafios/Desafio_035.py
+++ Desafios/Desafio_035.py
@@ -9,3 +9,5 @@ if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
     print('\nÉ um triângulo')
 else:
     print('\nNão é um triângulo')
+
+    print (3 * 5 + 4 ** 2)
