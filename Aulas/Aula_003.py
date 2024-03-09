f3855bc6f84fa11c6fada586635ff77bd6d0a905
João Andrade <andradezerd@gmail.com>
2024-03-09 13:27:22 -0300

    Aula 13 atualização

--- Aulas/Aula_003.py
+++ Aulas/Aula_003.py
@@ -1,6 +1,6 @@
-num1 = int(input('Digite um valor: '))
+num1 = int(input('\nDigite um valor: '))
 num2 = int(input('Digite um novo valor: '))
 
 resultado = num1 + num2
 
-print (f'A soma de {num1} e {num2} é igual a {resultado}')
+print (f'\nA soma de {num1} e {num2} é igual a {resultado}')
--- Aulas/Aula_013.py
+++ Aulas/Aula_013.py
@@ -3,6 +3,6 @@ n2 = float(input('Digite a segundo nota: '))
 media = (n1 + n2)/2
 print (f'A sua média foi {media:.1f}')
 if media >= 6.0:
-    print ('Sua me´dia foi boa! PARABÉNS')
+    print ('Sua meédia foi boa! PARABÉNS')
 else:
     print ('Sua média foi ruim! ESTUDE MAIS')
