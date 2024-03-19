15fc1b3fa6c59196fc094899945076a0ee26a629
João Andrade <andradezerd@gmail.com>
2024-03-19 16:40:10 -0300

    Update Desafio_065.py

--- Desafios/Desafio_065.py
+++ Desafios/Desafio_065.py
@@ -2,4 +2,21 @@
 # E no final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
 # O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
-a
+resp = 'S'
+soma = quantidade = media = maior = menor = 0
+
+while resp in 'Ss':
+    num = int(input('\nDigite um número: '))
+    soma += num
+    quantidade += 1
+    if quantidade == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quantidade
+print (f'\nVocê digitou {quantidade} números e a média foi de {media}!')
+print (f'MAIOR valor: {maior}\nMENOR valor: {menor}')
