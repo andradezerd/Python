ae5dc79731ed9fbd64748a0b2f30aebe04fc098e
João Andrade <andradezerd@gmail.com>
2024-03-28 13:31:40 -0300

    Update Desafio_071.py

--- Desafios/Desafio_071.py
+++ Desafios/Desafio_071.py
@@ -3,3 +3,25 @@
 # E o programa vai informar quantas cédulas de cada valor serão entregues. 
 # OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 
+print ('=' * 30)
+print ('{:^30}'.format('BANCO IMAGINÁRIO'))
+print ('=' * 30)
+valor = int(input('Valor de Saque: R$'))
+total = valor
+cedula = 50
+tot_cedula = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        tot_cedula += 1
+    else:
+        print (f'Total de {tot_cedula} cédulas de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        tot_cedula = 0
+        if total == 0:
+            break
