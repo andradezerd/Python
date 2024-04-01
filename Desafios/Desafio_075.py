49e57fd12854ac7aa85151a60776dcfb593e8dfc
João Andrade <andradezerd@gmail.com>
2024-04-01 16:37:28 -0300

    Update Desafio_075.py

--- Desafios/Desafio_075.py
+++ Desafios/Desafio_075.py
@@ -3,3 +3,18 @@
 # A) Quantas vezes apareceu o valor 9.
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
+
+num = (int(input('Digite o primeiro número: ')), 
+       int(input('Digite o segundo número: ')),
+       int(input('Digite o terceiro número: ')),
+       int(input('Digite o último número: ')))
+print (f'\nVocê digitou os valores {num}')
+print (f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print (f'O valor 3 apareceu na {num.index(3)+1}ª posição')
+else:
+    print ('O valor 3 não foi digitado em nenhuma posição')
+print (f'Os números pares foram ', end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
