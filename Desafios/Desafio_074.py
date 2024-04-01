f97084d2cef65578c724d94713e2fb0e3418082d
João Andrade <andradezerd@gmail.com>
2024-04-01 16:20:04 -0300

    Update Desafio_074.py

--- Desafios/Desafio_074.py
+++ Desafios/Desafio_074.py
@@ -3,5 +3,10 @@
 
 from random import randint
 
-n = (randint(1, 10), randint(1, 10), randint(1, 10) , randint(1, 10), randint(1, 10))
-print (f'Eu sorteei os valores {n}')
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10) , randint(1, 10), randint(1, 10))
+
+print ('\nOs valores sorteados foram: ', end='')
+for n in numeros: 
+    print (f'{n} ', end='')
+print (f'\nO maior valor sorteado foi {max(numeros)}')
+print (f'O menor valor sorteado foi {min(numeros)}')
